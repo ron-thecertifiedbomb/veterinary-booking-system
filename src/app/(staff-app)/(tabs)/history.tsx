70786b2a919161740a53d6d0ca0fcd9ef289461c
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2026-06-17 05:02:03 +0800

    refactor: enhance History component with appointment fetching and display; implement loading state and improve layout with FlatList for better user experience

--- src/app/(staff-app)/(tabs)/history.tsx
+++ src/app/(staff-app)/(tabs)/history.tsx
@@ -1,15 +1,18 @@
+import AppointmentCard from "@/components/common/Appointments/AppointmentCard";
+import Loader from "@/components/common/Loader/Loader";
 import { useGetStaffAppointments } from "@/features/appointment/hooks/useGetStaffAppointments";
 import { useAuth } from "@/features/auth/providers/AuthProvider";
 import { useEffect } from "react";
-import { Text } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function History() {
 
 const {token, user } = useAuth()
-const {loading, fetchStaffAppointments , setFilters} = useGetStaffAppointments()
+const {loading, appointments, fetchStaffAppointments} = useGetStaffAppointments()
 
 useEffect(() => {
+
     if (!token) {
       return
     }
@@ -18,10 +21,32 @@ useEffect(() => {
 
 
     return (
-        <SafeAreaView className="flex-1 bg-background justify-center items-center px-6">
-            <Text className="text-lg font-semibold mb-2">
-            History
-            </Text>
+        <SafeAreaView className="flex-1 bg-background">
+             <View className="flex-1 px-2">
+                    <FlatList
+                        data={appointments}
+                        keyExtractor={(item) => item.bookingCode}
+                        showsVerticalScrollIndicator={false}
+                        contentContainerStyle={{
+                            paddingBottom: 32,
+                            paddingTop: 8,
+                        }}
+                        onRefresh={fetchStaffAppointments}
+                        refreshing={loading}
+                        ListFooterComponent={
+                            loading ? (
+                                <View className="py-4">
+                                    <Loader />
+                                </View>
+                            ) : (
+                                <View style={{ height: 40 }} />
+                            )
+                        }
+                        renderItem={({ item }) => (
+                            <AppointmentCard appointments={item} />
+                        )}
+                    />
+                </View>
         </SafeAreaView>
     );
 }
